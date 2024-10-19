48bde5fa31a190ddc154c58a7e5be40b8c143bf4
jawon <jpointw@gmail.com>
2024-10-19 12:18:24 +0900

    Mine Spawn minDistance Add

--- Assets/@Scripts/MineSystem.cs
+++ Assets/@Scripts/MineSystem.cs
@@ -2,62 +2,46 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
-using Random = UnityEngine.Random; // Unity의 Object Pool 네임스페이스
 
 public class MineSystem : SingletonMonoBase<MineSystem>
 {
-    public 
-    
-    public MineBase goldMinePrefab;     
-    public MineBase coalMinePrefab;     
-    public MineBase ironMinePrefab;     
-    public MineBase diamondMinePrefab;  
+    public GameObject plane;
 
-    private ObjectPool<MineBase> goldMinePool;    
-    private ObjectPool<MineBase> coalMinePool;    
-    private ObjectPool<MineBase> ironMinePool;    
-    private ObjectPool<MineBase> diamondMinePool; 
+    public MineBase goldMinePrefab;
+    public MineBase coalMinePrefab;
+    public MineBase ironMinePrefab;
+    public MineBase diamondMinePrefab;
 
-    public List<MineBase> activeMines; 
+    public List<MineBase> activeMines;
+
+    private Dictionary<MineType, ObjectPool<MineBase>> minePools;
 
     void Start()
     {
-        goldMinePool = new ObjectPool<MineBase>(
-            createFunc: () => Instantiate(goldMinePrefab),
-            actionOnGet: mine => {
-                mine.gameObject.SetActive(true);
-                activeMines.Add(mine);
-            },
-            actionOnRelease: mine => {
-                mine.ResetMine();
-                mine.gameObject.SetActive(false);
-                activeMines.Remove(mine);
-            },
-            actionOnDestroy: mine => Destroy(mine.gameObject),
-            defaultCapacity: 3,
-            maxSize: 5         
-        );
+        minePools = new Dictionary<MineType, ObjectPool<MineBase>>()
+        {
+            { MineType.GOLD, CreateMinePool(goldMinePrefab, 5, 10) },
+            { MineType.COAL, CreateMinePool(coalMinePrefab, 5, 10) },
+            { MineType.IRON, CreateMinePool(ironMinePrefab, 5, 10) },
+            { MineType.DIAMOND, CreateMinePool(diamondMinePrefab, 5, 10) }
+        };
+    }
 
-        coalMinePool = new ObjectPool<MineBase>(
-            createFunc: () => Instantiate(coalMinePrefab),
-            actionOnGet: mine => {
-                mine.gameObject.SetActive(true);
-                activeMines.Add(mine);
-            },
-            actionOnRelease: mine => {
-                mine.ResetMine();
-                mine.gameObject.SetActive(false);
-                activeMines.Remove(mine);
-            },
-            actionOnDestroy: mine => Destroy(mine.gameObject),
-            defaultCapacity: 3,
-            maxSize: 5
-        );
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RequestNewMine(plane.transform.position, 4);
+        }
+    }
 
-        ironMinePool = new ObjectPool<MineBase>(
-            createFunc: () => Instantiate(ironMinePrefab),
+    private ObjectPool<MineBase> CreateMinePool(MineBase prefab, int defaultCapacity, int maxSize)
+    {
+        return new ObjectPool<MineBase>(
+            createFunc: () => Instantiate(prefab),
             actionOnGet: mine => {
                 mine.gameObject.SetActive(true);
+                Debug.LogError("Created Mine");
                 activeMines.Add(mine);
             },
             actionOnRelease: mine => {
@@ -66,66 +50,67 @@ public class MineSystem : SingletonMonoBase<MineSystem>
                 activeMines.Remove(mine);
             },
             actionOnDestroy: mine => Destroy(mine.gameObject),
-            defaultCapacity: 2,
-            maxSize: 4
-        );
-
-        diamondMinePool = new ObjectPool<MineBase>(
-            createFunc: () => Instantiate(diamondMinePrefab),
-            actionOnGet: mine => {
-                mine.gameObject.SetActive(true);
-                activeMines.Add(mine);
-            },
-            actionOnRelease: mine => {
-                mine.ResetMine();
-                mine.gameObject.SetActive(false);
-                activeMines.Remove(mine);
-            },
-            actionOnDestroy: mine => Destroy(mine.gameObject),
-            defaultCapacity: 1,
-            maxSize: 2
+            defaultCapacity: defaultCapacity,
+            maxSize: maxSize
         );
     }
 
-    public MineBase RequestNewMine(Vector3 position)
+    public MineBase RequestNewMine(Vector3 planePosition, float minDistanceBetweenMines)
     {
         MineBase newMine = null;
-
-        int randomValue = Random.Range(0, 100);
+        int randomValue = UnityEngine.Random.Range(0, 100);
 
         newMine = randomValue switch
         {
-            < 40 => goldMinePool.Get(),
-            < 70 => coalMinePool.Get(),
-            < 90 => ironMinePool.Get(),
-            _ => diamondMinePool.Get()
+            < 40 => minePools[MineType.GOLD].Get(),
+            < 70 => minePools[MineType.COAL].Get(),
+            < 90 => minePools[MineType.IRON].Get(),
+            _ => minePools[MineType.DIAMOND].Get()
         };
 
         if (newMine != null)
         {
-            newMine.transform.position = position;
+            int maxAttempts = 10;
+            bool validPosition = false;
+
+            for (int attempts = 0; attempts < maxAttempts && !validPosition; attempts++)
+            {
+                Vector3 randomPosition = GenerateRandomPositionOnPlane(planePosition);
+
+                Collider[] hitColliders = Physics.OverlapSphere(randomPosition, minDistanceBetweenMines, 6);
+
+                if (hitColliders.Length == 0)
+                {
+                    newMine.transform.position = randomPosition;
+                    validPosition = true;
+                }
+            }
+
+            if (!validPosition)
+            {
+                Debug.LogWarning("Failed to find valid position for the new mine after multiple attempts.");
+                minePools[newMine.mineType].Release(newMine);
+                newMine = null;
+            }
         }
 
         return newMine;
     }
 
+    private Vector3 GenerateRandomPositionOnPlane(Vector3 planePosition)
+    {
+        Vector3 planeScale = plane.transform.localScale;
+
+        float randomX = UnityEngine.Random.Range(-0.5f * planeScale.x * 10, 0.5f * planeScale.x * 10);
+        float randomZ = UnityEngine.Random.Range(-0.5f * planeScale.z * 10, 0.5f * planeScale.z * 10);
+
+        return new Vector3(planePosition.x + randomX, planePosition.y, planePosition.z + randomZ);
+    }
+
     public void ReturnMineToPool(MineBase mine)
     {
-        switch (mine.mineType)
-        {
-            case MineType.GOLD:
-                goldMinePool.Release(mine);
-                break;
-            case MineType.COAL:
-                goldMinePool.Release(mine);
-                break;
-            case MineType.IRON:
-                goldMinePool.Release(mine);
-                break;
-            case MineType.DIAMOND:
-                goldMinePool.Release(mine);
-                break;
-        }
+        if (minePools.ContainsKey(mine.mineType))
+            minePools[mine.mineType].Release(mine);
     }
 
     public MineBase GetClosestMine(Vector3 minerPosition)
@@ -133,7 +118,7 @@ public class MineSystem : SingletonMonoBase<MineSystem>
         MineBase closestMine = null;
         float closestDistance = Mathf.Infinity;
 
-        foreach (MineBase mine in activeMines)
+        foreach (var mine in activeMines)
         {
             if (mine.IsTargeted || mine.IsConsumed) continue;
 
@@ -146,9 +131,7 @@ public class MineSystem : SingletonMonoBase<MineSystem>
         }
 
         if (closestMine != null)
-        {
             closestMine.MineTargeted(true);
-        }
 
         return closestMine;
     }
