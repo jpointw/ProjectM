c99ea1130e8fe3d8eaf20ef8215636176b4e0583
jawon <jpointw@gmail.com>
2024-12-14 03:07:34 +0900

    Extension 수정

--- Assets/@Scripts/Extensions/MinerExtensions.cs
+++ Assets/@Scripts/Extensions/MinerExtensions.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
-public class MinerExtensions
+public static class MinerExtensions
 {
-    
+    public static int GetDamage()
+    {
+        var modelDatas = GameSystems.ModelDatas;
+        var saveData = GameSystems.Data.SaveData;
+        int baseDamage = modelDatas.characterInfoDatas[0].Damage;
+
+        int levelBonusDamage = saveData.level * 15;
+
+        int toolBonusDamage = saveData.miningToolLevel[saveData.currentMiningToolID] * 15;
+
+        int totalDamage = baseDamage + levelBonusDamage + toolBonusDamage;
+        
+        return totalDamage;
+    }
+
+    public static float GetSpped()
+    {
+        var modelDatas = GameSystems.ModelDatas;
+        var saveData = GameSystems.Data.SaveData;
+        float speed = modelDatas.characterInfoDatas[0].Speed;
+
+        float levelBonusSpeed = saveData.level * 0.1f;
+
+        float toolBonusSpeed = saveData.miningToolLevel[saveData.currentMiningToolID] * 0.1f;
+        
+        float totalSpeed = speed + levelBonusSpeed + toolBonusSpeed + toolBonusSpeed;
+        
+        return totalSpeed;
+    }
 }
